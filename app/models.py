ae5b464f6b7e0df7aa78c75379f6d090614f9ff5
Owen Wang <owenwang0099@gmail.com>
2023-12-24 20:01:50 -0500

    renamed fields, added custom primarykey

--- app/models.py
+++ app/models.py
@@ -4,24 +4,25 @@ import os
 
 # Create your models here.
 class Playlist(models.Model):
-    title = models.CharField(max_length=64)
+    id = models.CharField(max_length=64, primary_key=True)
+    name = models.CharField(max_length=64)
     owner = models.CharField(max_length=64)
     platform = models.CharField(max_length=64)
-    link = models.URLField(max_length=200)
     thumbnail = models.ImageField(
         upload_to=os.path.join("playlist_history", "thumbnails"), blank=True
     )
 
     def __str__(self):
-        return f"{self.platform}: {self.title}"
+        return f"{self.platform}: {self.name}"
 
 
-class Song(models.Model):
+class Track(models.Model):
+    id = models.CharField(max_length=64, primary_key=True)
     name = models.CharField(max_length=64)
     artist = models.CharField(max_length=64)
     playlist = models.ForeignKey(
-        Playlist, on_delete=models.CASCADE, related_name="songs"
+        Playlist, on_delete=models.CASCADE, related_name="tracks"
     )
 
     def __str__(self):
-        return f"{self.playlist.title}: {self.name} by {self.artist}"
+        return f"{self.playlist.name}: {self.name} by {self.artist}"
