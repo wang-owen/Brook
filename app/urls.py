115acf82a45eb75ce34fcc1379a3ad70f0298c08
Owen Wang <owenwang0099@gmail.com>
2023-12-25 16:49:00 -0500

    styling

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("download", views.download, name="download"),
+    path("download/<str:playlist_id>", views.download, name="download"),
     path("update/<str:playlist_id>", views.update, name="update"),
+    path("remove/<str:playlist_id>", views.remove, name="remove"),
     path(
         "playlist/<str:playlist_platform>/<str:playlist_id>",
         views.playlist,
