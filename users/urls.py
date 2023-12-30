646ee54217db3dc281ab9ffe1640392b5e7ee7df
Owen Wang <owenwang0099@gmail.com>
2023-12-30 15:40:29 -0500

    login wip

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("clear-files", views.clear_files, name="clear-files"),
-    path("logout", views.logout_view, name="logout"),
+    path("register/", views.register_view, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("clear-files/", views.clear_files, name="clear-files"),
 ]
--- users/views.py
+++ users/views.py
@@ -9,16 +9,24 @@ from shutil import rmtree
 
 
 # Create your views here.
+def register_view(request):
+    return render(request, "users/register.html")
+
+
+def login_view(request):
+    return render(request, "users/login.html")
+
+
 @login_required
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
 def clear_files(request):
     # Only allow superusers to clear files
     if request.user.is_superuser:
         # Delete all files in the music directory
         if Path(settings.MUSIC_DIR).exists():
             rmtree(settings.MUSIC_DIR)
-    return HttpResponseRedirect(reverse("index"))
-
-
-def logout_view(request):
-    logout(request)
     return HttpResponseRedirect(reverse("index"))
