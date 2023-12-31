c5d456eeb4363c877fd5d4683c9d396d20df79f8
Owen Wang <owenwang0099@gmail.com>
2023-12-31 13:02:42 -0500

    fix please

--- Brook/settings.py
+++ Brook/settings.py
@@ -15,16 +15,14 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 
-load_dotenv()
-MUSIC_DIR = Path("Music")
-
-MEDIA_URL = "/Music/"
-MEDIA_ROOT = os.path.join('Music')
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+load_dotenv()
+MUSIC_DIR = Path("Music")
+MEDIA_URL = "/Music/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'Music')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
