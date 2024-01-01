e0004bf36803387a70bf79966790d1697c6a381a
Owen Wang <owenwang0099@gmail.com>
2024-01-01 18:58:26 -0500

    remove unecessasry urls

--- Brook/settings.py
+++ Brook/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 MUSIC_DIR = Path("Music")
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = BASE_DIR / "media"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -140,8 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = Path.joinpath(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = (Path.joinpath(BASE_DIR, STATIC_URL),)
+# STATIC_ROOT = Path.joinpath(BASE_DIR, "staticfiles")
+# STATICFILES_DIRS = (Path.joinpath(BASE_DIR, STATIC_URL),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- Brook/urls.py
+++ Brook/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("app.urls")),
     path("", include("users.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]  # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
