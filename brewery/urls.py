a7e081b8bc91de153d90e3b79a4bdd95ef1c5fb3
Owen Wang <owenwang0099@gmail.com>
2024-04-01 00:52:36 -0400

    urls.py (urls handled by client now)

--- brewery/urls.py
+++ brewery/urls.py
@@ -1,17 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path("brew", views.brew, name="brew"),
-    path("brew/<str:playlist_id>", views.brew, name="brew"),
-    path("download/<path:path>", views.download, name="download"),
-    path("get-playlists", views.get_playlists, name="get-playlists"),
-    path("update/<str:playlist_id>", views.update, name="update"),
-    path("remove/<str:playlist_id>", views.remove, name="remove"),
-    path(
-        "playlist/<str:playlist_platform>/<str:playlist_id>",
-        views.playlist,
-        name="playlist",
-    ),
-    path("watch", views.watch, name="watch"),
-]
+urlpatterns = []
