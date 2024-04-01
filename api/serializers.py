fe3ca4c2a850f68ed4477eb6c9c9a2ef9108801c
Owen Wang <owenwang0099@gmail.com>
2024-03-31 22:45:15 -0400

    model serializers

---
+++ api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from brewery.models import Playlist, Track
+from users.models import User
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "email", "date_joined"]
+
+
+class TrackSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Track
+        fields = "__all__"
+
+
+class PlaylistSerializer(serializers.HyperlinkedModelSerializer):
+    tracks = TrackSerializer(many=True)
+
+    class Meta:
+        model = Playlist
+        fields = "__all__"
