9c8ea3b1966f52dc1dc5169709368046c7a74d6b
Owen Wang <owenwang0099@gmail.com>
2024-03-31 22:46:31 -0400

    urls.py

--- Brook/urls.py
+++ Brook/urls.py
@@ -19,9 +19,17 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserViewSet)
+router.register(r"playlists", views.PlaylistViewSet)
+router.register(r"tracks", views.TrackViewSet)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("app.urls")),
-    path("", include("users.urls")),
+    path("api/", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
