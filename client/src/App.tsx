ccb63cec01a7d918a306ca4817afac9e1d0890cc
Owen Wang <owenwang0099@gmail.com>
2024-05-01 19:22:38 -0400

    app layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,12 +6,16 @@ import {
 } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import BrewPage from "./pages/BrewPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 const App = () => {
     const router = createBrowserRouter(
         createRoutesFromElements(
             <Route path="/" element={<MainLayout />}>
                 <Route index element={<BrewPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
             </Route>
         )
     );
