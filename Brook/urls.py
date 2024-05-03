d325938065d528bc233dcbe190bf644423817fa0
Owen Wang <owenwang0099@gmail.com>
2024-05-03 13:44:06 -0400

    simplify url paths

--- Brook/urls.py
+++ Brook/urls.py
@@ -19,19 +19,9 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework import routers
-from api import views
-
-router = routers.SimpleRouter()
-router.register(r"users", views.UserViewSet)
-router.register(r"playlists", views.PlaylistViewSet)
-router.register(r"tracks", views.TrackViewSet)
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("download/<path:path>", views.download),
-    path("brew/", views.brew),
-    path("api/", include(router.urls)),
+    path("", include("api.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
