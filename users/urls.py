94e4a1386a10a435568e272df13bd5b73d43a872
Owen Wang <owenwang0099@gmail.com>
2024-05-06 14:35:32 -0400

    rearrange files

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("register/", views.register_view, name="register"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
-    path("clear-files/", views.clear_files, name="clear-files"),
-]
