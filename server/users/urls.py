8152db0137f0e0af0469564e237fcd1e52c285cf
Owen Wang <owenwang0099@gmail.com>
2024-05-11 16:34:40 -0400

    more consistent login detection

--- server/users/urls.py
+++ server/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("get-logged-in", views.UserList.as_view()),
     path("register/", views.register_view),
     path("login/", views.login_view),
     path("logout/", views.logout_view),
