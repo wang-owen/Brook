f549882bde4feb38f2b7cb8acdb1f68464a53c5a
Owen Wang <owenwang0099@gmail.com>
2024-07-16 19:42:31 +0800

    added converter endpoint

--- server/server/urls.py
+++ server/server/urls.py
@@ -21,6 +21,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("brewery.urls")),
+    path("", include("converter.urls")),
     path("", include("users.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ]
