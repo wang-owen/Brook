14dfc91efde6bc1a1feaba706e25f1c8ba6fd0d9
Owen Wang <owenwang0099@gmail.com>
2024-07-24 19:04:20 +0800

    removed designated convert endpoints

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,10 @@ import BrewPage from "./pages/BrewPage";
 import ConvertPage from "./pages/ConvertPage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
+import ConvertToYouTubePage from "./pages/ConvertToYouTubePage";
 import ConvertToSpotifyPage from "./pages/ConvertToSpotifyPage";
+import ConvertToApplePage from "./pages/ConvertToApplePage";
+import ConvertToAmazonPage from "./pages/ConvertToAmazonPage";
 
 interface LoginContextProps {
     loggedIn: boolean;
@@ -53,10 +56,6 @@ const App = () => {
             <Route path="/" element={<MainLayout />}>
                 <Route index element={<BrewPage />} />
                 <Route path="/convert" element={<ConvertPage />} />
-                <Route
-                    path="/convert/spotify"
-                    element={<ConvertToSpotifyPage />}
-                />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/login" element={<LoginPage />} />
             </Route>
