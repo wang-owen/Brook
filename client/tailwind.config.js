711a3834a84f2305737bce5500427276ed78850a
Owen Wang <owenwang0099@gmail.com>
2024-12-03 16:13:26 -0500

    daisyui require bugfix

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -32,5 +34,5 @@ export default {
             },
         },
     },
-    plugins: [require("daisyui")],
+    plugins: [daisyui],
 };
